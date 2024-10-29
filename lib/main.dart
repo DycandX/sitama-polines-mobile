3d4820f36545f6c5440ea631becf333bd80238fe
DycandX <karismaviki@gmail.com>
2024-10-29 15:57:08 +0700

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: WelcomePage(),
+      //home: WelcomePage(),
     );
   }
 }
