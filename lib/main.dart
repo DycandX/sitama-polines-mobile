d52a11b74b3d8b29468a9f81cf15cfe70392ff28
DycandX <karismaviki@gmail.com>
2024-10-31 23:31:28 +0700

    testing modul 9

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: DaftarBimbingan(),
+      //home: LoginPage(),
     );
   }
 }
