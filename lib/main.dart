28dea475c79d40130c421aef056c96298240f272
DycandX <karismaviki@gmail.com>
2024-11-01 11:27:53 +0700

    menambahkan module sidang tugas akhir -anin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pbl_sitama/modules/02_login/login_page.dart';
+import 'package:pbl_sitama/modules/07_sidang_tugas_akhir/sidang_ta_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,9 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomePage(),
+      home: SidangScreen(),
     );
   }
 }
---
+++ lib/modules/07_sidang_tugas_akhir/sidang_ta_screen.dart
@@ -0,0 +1,200 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, sort_child_properties_last, prefer_const_constructors_in_immutables, library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SidangScreen(),
+    );
+  }
+}
+
+class SidangScreen extends StatefulWidget {
+  @override
+  _SidangScreenState createState() => _SidangScreenState();
+}
+
+class _SidangScreenState extends State<SidangScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 10,
+        toolbarHeight: 10,
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Transform.translate(
+                  offset: Offset(-20, 0),
+                  child: RawMaterialButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    elevation: 2.0,
+                    fillColor: Colors.indigo[900],
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 15.0,
+                      color: Colors.white,
+                    ),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(),
+                  ),
+                ),
+                Spacer(),
+                Row(
+                  children: [
+                    SizedBox(width: 30),
+                    Text(
+                      'Adnan Bima Adhi N',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    SizedBox(width: 10),
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundColor: Colors.grey[200],
+                      child: Icon(Icons.person, size: 30, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Text(
+              'Sidang',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87),
+            ),
+            Text(
+              'Tugas Akhir',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87),
+            ),
+            SizedBox(height: 20),
+            CustomExpansionCard(
+              title: 'Data Sidang Tugas Akhir',
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: Column(
+                    children: [
+                      CustomTextField(label: 'Pembimbing 1', isEditable: false),
+                      SizedBox(height: 10),
+                      CustomTextField(label: 'Pembimbing 2', isEditable: false),
+                      SizedBox(height: 10),
+                      CustomTextField(label: 'Penguji', placeholderText: 'Belum Di Plotting'),
+                      SizedBox(height: 10),
+                      CustomTextField(label: 'Sekretaris', placeholderText: 'Belum Di Plotting'),
+                      SizedBox(height: 10),
+                      CustomTextField(label: 'Tahun Akademik', isEditable: false),
+                      SizedBox(height: 10),
+                      CustomTextField(label: 'Judul Tugas Akhir', maxLines: 2, isEditable: false),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            CustomExpansionCard(
+              title: 'Status',
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: Column(
+                    children: [
+                      CustomTextField(label: 'Hari/Tanggal', isEditable: false),
+                      SizedBox(height: 10),
+                      CustomTextField(label: 'Ruangan', isEditable: false),
+                      SizedBox(height: 10),
+                      CustomTextField(label: 'Sesi', isEditable: false),
+                      SizedBox(height: 10),
+                      CustomTextField(label: 'Status Sidang', placeholderText: 'Belum melaksanakan sidang'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomExpansionCard extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+
+  CustomExpansionCard({required this.title, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: ExpansionTile(
+        leading: Icon(Icons.menu, size: 30, color: Colors.black),
+        title: Text(
+          title,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        children: children,
+        trailing: Icon(Icons.arrow_drop_down, size: 30, color: Colors.black),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final int maxLines;
+  final String? placeholderText;
+  final bool isEditable;
+
+  CustomTextField({required this.label, this.maxLines = 1, this.placeholderText, this.isEditable = true});
+
+  @override
+  Widget build(BuildContext context) {
+    bool isPlaceholder = placeholderText != null;
+    return TextField(
+      maxLines: maxLines,
+      readOnly: !isEditable || isPlaceholder,
+      controller: isPlaceholder ? TextEditingController(text: placeholderText) : null,
+      style: isPlaceholder ? TextStyle(fontStyle: FontStyle.italic, color: Colors.red) : null,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+}
