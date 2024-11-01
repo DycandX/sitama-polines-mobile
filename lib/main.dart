115b47b205e680751e21b7fccdea52270dfaeaad
DycandX <karismaviki@gmail.com>
2024-11-01 17:56:41 +0700

    update module sidang ta

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:pbl_sitama/modules/03_home_mahasiswa/home_mahasiswa_screen.dart'
 import 'package:pbl_sitama/modules/04_dashboard_tugas_akhir/dasboard_ta_tampilkan.dart';
 import 'package:pbl_sitama/modules/05_pembimbingan/pembimbingan_screen.dart';
 import 'package:pbl_sitama/modules/06_daftar_tugas_akhir/daftar_ta_screen.dart';
-import 'package:pbl_sitama/modules/07_sidang_tugas_akhir/sidang_ta_screen.dart';
+import 'package:pbl_sitama/modules/07_sidang_tugas_akhir/sidang_tugas_akhir/sidang_ta_screen.dart';
 import 'package:pbl_sitama/modules/08_home_dosen/home_dosen_screen.dart';
 //import 'package:pbl_sitama/modules/04_dashboard_tugas_akhir/dashboard_TA_tampilan.dart';
 //import 'package:pbl_sitama/modules/04_dashboard_tugas_akhir/dasboard_ta_tampilkan.dart';
@@ -20,7 +20,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       
-      home: SidangTaScreen(),
+      home: WelcomePage(),
+      //home: SidangTaScreen(),
       //home: DaftarTaScreen(),
       //home: PembimbinganScreen(),
       //home: JadwalSidangPage(),
