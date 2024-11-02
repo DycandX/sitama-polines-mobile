7bd3a9c8a9b910858bfabc3b53c91176c5380415
DycandX <karismaviki@gmail.com>
2024-11-03 00:28:48 +0700

    remove unused import, kurang module untuk dosen, module 8,9a,9b (bagian navbar gunakan dari module 9 mahasiswa bimbingan)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pbl_sitama/modules/01_launch/welcome_page.dart';
-import 'package:pbl_sitama/modules/08_home_dosen/home_dosen_screen.dart';
-import 'package:pbl_sitama/modules/09_tugas_akhir_dosen/mahasiswa_bimbingan/mahasiswa_bimbingan.dart';
-import 'package:pbl_sitama/modules/09_tugas_akhir_dosen/sidang_tugas_akhir/sidang_ta_dosen_screen_pembimbing.dart';
-//import 'package:pbl_sitama/modules/05_pembimbingan/pembimbingan_screen.dart';
-//import 'package:pbl_sitama/modules/06_daftar_tugas_akhir/daftar_ta_screen.dart';
-//import 'package:pbl_sitama/modules/09_tugas_akhir_dosen/mahasiswa_bimbingan/mahasiswa_bimbingan.dart';
-//import 'package:pbl_sitama/modules/02_login/login_page.dart';
-//import 'package:pbl_sitama/modules/03_home_mahasiswa/home_mahasiswa_screen.dart';
-//import 'package:pbl_sitama/modules/04_dashboard_tugas_akhir/dasboard_ta_tampilkan.dart';
-//import 'package:pbl_sitama/modules/05_pembimbingan/pembimbingan_screen.dart';
-//import 'package:pbl_sitama/modules/06_daftar_tugas_akhir/daftar_ta_screen.dart';
-//import 'package:pbl_sitama/modules/07_sidang_tugas_akhir/sidang_tugas_akhir/sidang_ta_screen.dart';
-//import 'package:pbl_sitama/modules/08_home_dosen/home_dosen_screen.dart';
-//import 'package:pbl_sitama/modules/04_dashboard_tugas_akhir/dashboard_TA_tampilan.dart';
-//import 'package:pbl_sitama/modules/04_dashboard_tugas_akhir/dasboard_ta_tampilkan.dart';
+
 
 
 void main() => runApp(const MyApp());
@@ -28,9 +14,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       
       //home: HomeScreen(),
-      home: MahasiswaBimbingan(),
+      //home: MahasiswaBimbingan(),
       //home: PembimbinganScreen(),
-      //home: WelcomePage(),
+      home: WelcomePage(),
       //home: SidangTaScreen(),
       //home: DaftarTaScreen(),
       //home: PembimbinganScreen(),
